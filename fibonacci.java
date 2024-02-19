2c97adfd416b5b943fd897118dc61c40a26482a5
Jerome Wilson <jeromexy133@gmail.com>
2024-02-19 12:22:57 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -11,9 +11,7 @@ public class Fibonacci {
         if (n <= 1) {
             return n;
         }
-        int fib = 1;
-        int prevFib = 1;
-        for (int i = 2; i < n; i++) {
+        {
             int temp = fib;
             fib += prevFib;
             prevFib = temp;
