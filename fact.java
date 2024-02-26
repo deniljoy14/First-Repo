691b2e895480a364869a77266bcbcb46beeec6b4
Jerome Wilson <jeromexy133@gmail.com>
2024-02-26 11:47:26 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -6,7 +6,7 @@ public class Factorial {
         if (n == 0)
             return 1;
         else
-            return n * factorial(n - 1);
+            
     }
 
     public static void main(String[] args) {
